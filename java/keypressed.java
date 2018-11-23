d2722ea28c24753d9fb844e3f5107984f318a065
demsp <dserp18@gmail.com>
2018-11-23 21:48:29 +0300

    Create keypressed.java

---
+++ java/keypressed.java
@@ -0,0 +1,83 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+void setup() {
+//public class Main {
+ // public static void main(String[] args) {
+    JFrame aWindow = new JFrame();
+
+    aWindow.setBounds(50, 100, 300, 300);
+    aWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JTextField typingArea = new JTextField(20);
+    typingArea.addKeyListener(new KeyListener() {
+      /** Handle the key typed event from the text field. */
+      public void keyTyped(KeyEvent e) {
+        displayInfo(e, "KEY TYPED: ");
+      }
+
+      /** Handle the key pressed event from the text field. */
+      public void keyPressed(KeyEvent e) {
+        displayInfo(e, "KEY PRESSED: ");
+      }
+
+      /** Handle the key released event from the text field. */
+      public void keyReleased(KeyEvent e) {
+        displayInfo(e, "KEY RELEASED: ");
+      }
+
+      protected void displayInfo(KeyEvent e, String s) {
+        String keyString, modString, tmpString, actionString, locationString;
+        int id = e.getID();
+        if (id == KeyEvent.KEY_TYPED) {
+          char c = e.getKeyChar();
+          keyString = "key character = '" + c + "'";
+        } else {
+          int keyCode = e.getKeyCode();
+          keyString = "key code = " + keyCode + " (" + KeyEvent.getKeyText(keyCode) + ")";
+        }
+
+        int modifiers = e.getModifiersEx();
+        modString = "modifiers = " + modifiers;
+        tmpString = KeyEvent.getModifiersExText(modifiers);
+        if (tmpString.length() > 0) {
+          modString += " (" + tmpString + ")";
+        } else {
+          modString += " (no modifiers)";
+        }
+
+        actionString = "action key? ";
+        if (e.isActionKey()) {
+          actionString += "YES";
+        } else {
+          actionString += "NO";
+        }
+
+        locationString = "key location: ";
+        int location = e.getKeyLocation();
+        if (location == KeyEvent.KEY_LOCATION_STANDARD) {
+          locationString += "standard";
+        } else if (location == KeyEvent.KEY_LOCATION_LEFT) {
+          locationString += "left";
+        } else if (location == KeyEvent.KEY_LOCATION_RIGHT) {
+          locationString += "right";
+        } else if (location == KeyEvent.KEY_LOCATION_NUMPAD) {
+          locationString += "numpad";
+        } else { // (location == KeyEvent.KEY_LOCATION_UNKNOWN)
+          locationString += "unknown";
+        }
+
+        System.out.println(keyString);
+        System.out.println(modString);
+        System.out.println(actionString);
+        System.out.println(locationString);
+      }
+
+    });
+    aWindow.add(typingArea);
+    aWindow.setVisible(true);
+ // }
+}
