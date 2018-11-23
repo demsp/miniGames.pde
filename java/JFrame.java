c5a009ef9e76e314a8038d3f46c9bd06fc844773
demsp <dserp18@gmail.com>
2018-11-24 01:12:55 +0300

    Create JFrame.java

---
+++ java/JFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class MyClass {
+public static void main (String [] args) {
+JFrame myWindow = new JFrame("Пробное окно");
+myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+myWindow.setSize(400, 300);
+myWindow.setVisible(true);
+}
+}
