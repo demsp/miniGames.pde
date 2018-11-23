ccdfcf02638998e3e798f1f4c127d97ff3f82267
demsp <dserp18@gmail.com>
2018-11-23 21:54:11 +0300

    Create openclass.java

---
+++ java/openclass.java
@@ -0,0 +1,19 @@
+public class Openclass { 
+public static int a=10; 
+public static int b=15; 
+} 
+  
+/* 
+public class HelloWorld{ 
+     
+    public static void main(String []args){ 
+        
+        int new_a =Openclass.a; 
+        int new_b = Openclass.b; 
+        System.out.println("Hello World"); 
+        System.out.println(new_a); 
+        System.out.println(new_b); 
+    } 
+} 
+*/ 
+  
