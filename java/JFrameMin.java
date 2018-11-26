68594d05d2f8f8746f2d0a10070abd591ee5ebde
demsp <dserp18@gmail.com>
2018-11-26 13:29:04 +0300

    Update and rename JFrame.java to JFrameMin.java

--- java/JFrameMin.java
+++ java/JFrameMin.java
@@ -1,6 +1,6 @@
 // http://math.sgu.ru/sites/chairs/prinf/materials/java/lesson8.htm
 import javax.swing.*;
-public class MyClass {
+public class JFrameMin {
 public static void main (String [] args) {
 JFrame myWindow = new JFrame("Пробное окно");
 myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
