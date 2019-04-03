08891bbf227c46f685fc87ebb7a1cee615d576f4
demsp <dserp18@gmail.com>
2019-04-03 20:58:31 +0300

    Update kt2_p5.js

--- knightTurn/kt2_p5.js
+++ knightTurn/kt2_p5.js
@@ -5,16 +5,14 @@ class Module {
     this.modColor=modColor;
   }
   // Custom method for drawing the object
-   mouseClick() {
-    
+   mouseClick() {    
    if (mouseX >= this.xO && mouseX <= this.xO+100 && 
       mouseY >= this.yO && mouseY <= this.yO+100) {
-   if (mouseIsPressed && (mouseButton == LEFT)) {
-        //this.modColor=200; 
-   storX=this.xO;
-    storY=this.yO; 
- if(bool_mouseReleased==true ) {this.modColor=255;} 
-        } 
+   if (overBox && mouseIsPressed && (mouseButton == LEFT)) {
+     storX=this.xO;
+      storY=this.yO; 
+    if(bool_mouseReleased == true) {this.modColor=200;} 
+         } 
       }
    }
   update() {
@@ -22,19 +20,15 @@ class Module {
     rect(this.xO, this.yO, unitSize, unitSize); 
   }
 }
-//  //  //  //  //  
-let bool_mouseReleased = false;
-let storX;
-let storY;
+//  //  //  // 
+let bool_mouseReleased=false;
+let storX=0, storY=0;
 
-let bx=0;
-let by=0;
+let bx=0, by=0;
 let boxSize = 100;
 let overBox = false;
 let locked = false;
-let xOffset = 0.0; 
-let yOffset = 0.0; 
-//  //  //  //  //  
+let xOffset = 0.0,yOffset = 0.0; 
 let unit = 100; // -> width / unit;
 let unitSize=100; 
 let count;
@@ -43,8 +37,7 @@ let mods = [];
 
 function setup () {
    createCanvas(500, 500);
- //bx = width/2.0;
- // by = height/2.0;
+   rectMode(CORNER);
  stroke(100);
  
  let wideCount = width / unit;
@@ -63,32 +56,32 @@ function draw() {
     mods[i].mouseClick();
     mods[i].update();    
    }
- //  //  //  //  //  //
+ // //  //  //  //
  // Test if the cursor is over the box 
-  if (mouseX > bx-boxSize && mouseX < bx+boxSize && 
-      mouseY > by-boxSize && mouseY < by+boxSize) {
+  if (mouseX > bx && mouseX < bx+boxSize && 
+      mouseY > by && mouseY < by+boxSize) {
     overBox = true;  
   } else {    overBox = false;  }
-  // fill the Unit 1x1
-  fill(255);
+  //draw mod 1x1
+  fill(200);
   rect(0,0,100,100);
- // Draw the Knight
-  rect(bx, by, boxSize, boxSize);
+ //draw the Knnifht
+ rect(bx, by, boxSize, boxSize);
   fill(50);
-  ellipse(bx+50,by+50,20,20);  
+   ellipse(bx+50,by+50,20,20);
 }
+//  //  //  //  //  //  //  //
  function mousePressed() {
   if(overBox) { 
     locked = true; 
-    fill(255, 255, 255);
-  } else {
+    } else {
     locked = false;
   }
   xOffset = mouseX-bx; 
   yOffset = mouseY-by; 
 }
  function mouseDragged() {
-  bool_mouseReleased=false; 
+  bool_mouseReleased=false;
   if(locked) {
     bx = mouseX-xOffset; 
     by = mouseY-yOffset; 
