cde3d0a95da12e8fd53f3e8b00f857c378453159
demsp <dserp18@gmail.com>
2019-06-20 21:08:57 +0300

    Update p5tree1.js

--- graph_map/tree/p5tree1.js
+++ graph_map/tree/p5tree1.js
@@ -1,31 +1,111 @@
-//Tree tree;
+// https://github.com/nature-of-code/NOC-S17-2-Intelligence-Learning/blob/master/week1-graphs/01_binary_tree/tree.js
+
 var tree;
 
 function setup() {
-noCanvas();
-//Node n = new Node(5);
-var n = new Node(5);
-tree=new Tree();
-tree.addNode(n);
-//console.log(Tree);
-  console.log(tree);
+ // noCanvas();
+createCanvas(500,500);
+
+  // New tree
+  tree = new Tree();
+
+  // Add ten random values
+  for (var i = 0; i < 10; i++) {
+    tree.addValue(floor(random(0, 100)));
+  }
+
+  // Traverse the tree 
+  tree.traverse();
+
+  // Search the tree for 10
+  var result = tree.search(10);
+  if (result == null) {
+    console.log('not found');
+  } else {
+    console.log(result);
+  }
+}
+//###########################
+function Node(val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
-//function draw() { }
+// Search the tree for a value
 
-function Tree(){
-this.root = null;
+Node.prototype.search = function(val) {
+  if (this.value == val) {
+    return this;
+  } else if (val < this.value && this.left != null) {
+    return this.left.search(val);
+  } else if (val > this.value && this.right != null) {
+    return this.right.search(val);
+  }
+  return null;
+};
+
+
+// Visit a node
+Node.prototype.visit = function() {
+  // Recursively go left
+  if (this.left != null) {
+    this.left.visit();
+  }
+  // Print out value
+  console.log(this.value);
+  noStroke();
+  text(this.value,this.x,this.y);
+  // Recursively go right
+  if (this.right != null) {
+    this.right.visit();
+  }
+};
+
+// Add a node
+Node.prototype.addNode = function(n) {
+  if (n.value < this.value) {
+    if (this.left == null) {
+      this.left = n;
+    } else {
+      this.left.addNode(n);
+    }
+  } else if (n.value > this.value) {
+    if (this.right == null) {
+      this.right = n;
+    } else {
+      this.right.addNode(n);
+    }
+  }
+};
+// end Node
+
+
+// Tree object
+function Tree() {
+  // Just store the root
+  this.root = null;
 }
 
-Tree.prototype.addNode = function(n){
-if(this.root==null){
-  this.root=n;
-   }
-}
+// Start by visiting the root
+Tree.prototype.traverse = function() {
+  this.root.visit();
+};
 
-function Node(val){
-  this.value=val;
- this.left = null;
- this.right = null;
- 
-}
+// Start by searching the root
+Tree.prototype.search = function(val) {
+  var found = this.root.search(val);
+  return found;
+};
+
+// Add a new value to the tree
+Tree.prototype.addValue = function(val) {
+  var n = new Node(val);
+  if (this.root == null) {
+    this.root = n;
+    this.root.x=width/2;
+    this.root.y=height/2;
+  } else {
+    this.root.addNode(n);
+  }
+};
